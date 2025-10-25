9525d238f4de69e60097ff3bddc2787177b773af
atigapeace-create <atigapeace@gmail.com>
2025-10-25 10:14:17 -0700

    Real Deriv trading bot with live market

--- app.py
+++ app.py
@@ -1,6 +1,1 @@
-import os
-import random
-import time
-import threading
-from datetime import datetime
-from flask import Flask, render_template_string, request, jsonify
+[PASTE THE ENTIRE REAL DERIV BOT CODE ABOVE]
